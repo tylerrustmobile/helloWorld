a0a7fcef00e524d4f39d2f8c7bcfa8c3a47dee8e
Tyler Rust Mobile <70029510+tylerrustmobile@users.noreply.github.com>
2020-08-24 10:01:46 -0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace helloWorld
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        int count = 0;
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+
+        }
+    }
+}
